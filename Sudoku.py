18d8b9fb225ee44c986b1f67a003ff6e56844831
Lucas Boulanger <lucas@opalonline.ca>
2018-10-30 00:08:34 -0400

    AC3 implemented

--- Sudoku.py
+++ Sudoku.py
@@ -1,65 +1,145 @@
+from queue import Queue
+
 class SudokuCSP():
     '''
-        - self.domain -> 3d list with [row][col][domain for variable[row][col]]
-        - self.values -> 2d array of current sudoku values. 
-                         if [row][col] == "*", then no number placed there, 
-                         if [row][col] == (some int), then domain[row][col] = [(some int)] 
-        - self.constraints -> tbd
+        domain = dictionary where Key = (x, y), Value = [domain]
+        arcs   = dictionary where Key = (x, y), Value = [Neighbors...]
     '''
     def __init__(self, file):
-        self.domain = []
-        self.values = []
-        self.arcs = []
-        row = 0
+        self.domain = {}
+        self.arcs = {}
+        x = 0
+        y = 0
         for line in file:
-            self.domain.append([])
-            self.values.append([])
             for char in line:
                 if char != "\n":
-                    self.values[row].append(char)
+                    self.arcs[(x,y)] = []
                     if char == "*":
-                        self.domain[row].append([1,2,3,4,5,6,7,8,9])
+                        self.domain[(x,y)] = [1,2,3,4,5,6,7,8,9]
                     else:
-                        self.domain[row].append([int(char)])
-            row += 1
-
+                        self.domain[(x,y)] = [int(char)]
+                y += 1
+            x += 1
+            y = 0
+        self.populateArcs()    
+        return    
+    
+    '''
+    Initial list of all possible arcs.
+    Arcs are expressed as dictionary where Key = (x, y), Value = [Neighbors...]
+        - thus all values in the sudoku puzzle are given arcs to their neighbors
+    '''
+    def populateArcs(self):
+        for key in self.arcs:
+            for i in range(0,9):
+                for j in range(0,9):
+                    if (i != key[0] or j != key[1]) and (i == key[0] or j == key[1] or (i in range((key[0]//3)*3,(key[0]//3)*3+3) and j in range((key[1]//3)*3,(key[1]//3)*3+3))):
+                        self.arcs[key].append((i,j))              
+        total = 0
+        for key in self.arcs: 
+            self.arcs[key] = list(set(self.arcs[key]))
+        for key in self.arcs:
+            total += len(self.arcs[key])
+        
+        print(total)
+        return  
+        
     '''
     Since all constraints are between two integers, 
     I think we can just define this function to 
     compare them.
     Anytime you need to check constraints on two 
-    variables, just send them here.
+    variables, just send the arc here.
     '''
-    def constraintSatisfy(self, Xi, Xj):
+    def constraintCheck(self, Xi, Xj):
         return Xi != Xj
-        
-    def getRowNeighbors(self):
-        return []
-    
-    def getColNeighbors(self):
-        return []
-    
-    def getBoxNeighbors(self):
-        return []
       
+    '''
+    Prints domains, for debugging
+    '''
     def printDomain(self):
-        for i in range (0,9):
-            for j in range (0,9):
-                print(self.domain[i][j], end="")
-            print()
-            
+        for key in self.domain:
+            print("{} -> {}".format(key, self.domain[key]))
+        return
+      
+    def isSolved(self):
+        for key in self.domain:
+            if len(self.domain[key]) != 1:
+                return False
+        return True
+    
+    '''
+    Prints the current sudoku problem by the values
+    in the familiar sudoku format.
+    ''' 
     def __str__(self):
         s = ""
-        for i in range (0,9):
-            s += "|"
-            for j in range(0,9):
-                s+= self.values[i][j] + "|"
-            s += "\n"
-        return s
-        
+        count = 0
+        rows = 0
+        for key in self.domain:
+            if (rows == 0 or rows == 3 or rows == 6) and count == 0:
+                s += "-------------\n"
+            if count == 0 or count == 3 or count == 6:
+                s += "|"
+            if len(self.domain[key]) == 1:
+                s += str(self.domain[key][0])
+            else:
+                s += "*"
+            count += 1
+            if count == 9:
+                s += "|\n"
+                rows += 1
+                count = 0
+        return s + "-------------\n"
 
+'''
+Main AC-3 algorithm. 
+Returns True or False dependent upon whether the algorithm failed or not.
+'''        
+def AC3_Main(csp):   
+    #initiate a queue to store all current arcs
+    queue = Queue()
+    for key in csp.arcs:
+        for neighbor in csp.arcs[key]:
+            queue.put((key, neighbor))
+    
+    #iterate through queue until it isnt empty
+    while not queue.empty():
+        i,j = queue.get()
+        if AC3_Revise(csp, i,j):
+            if len(csp.domain[i]) == 0:
+                return False
+            for key in csp.arcs[i]:
+                queue.put((key, i))
+    return True
+    
+def AC3_Revise(csp, i, j):
+    revised = False
+    tbr = []         #to be removed
+    for x in csp.domain[i]:
+        valid = False
+        for y in csp.domain[j]:
+            if csp.constraintCheck(x,y):
+                valid = True
+        if not valid:
+            tbr.append(x)
+            revised = True
+    if revised:
+        for x in tbr:
+            csp.domain[i].remove(x)
+    return revised
+        
 sudoku = SudokuCSP(open("data\Testing1.txt", "r"))
 print("---------------------------\nCurrent Sudoku Problem: \n---------------------------")
 print(sudoku)
+print("Sudoku solved? {}\n".format(sudoku.isSolved()))
+if not sudoku.isSolved():
+    print("Attempting AC-3 Algorithm...")
+    if not AC3_Main(sudoku):
+        print("Could not solve sudoku, one or more domain became empty.\n")
+    else:
+        ("Sudoku solved? {}\n".format(sudoku.isSolved()))
+        print(sudoku)
 #print("---------------------------\nDomain for each Variable: \n---------------------------")
-s#udoku.printDomain()
+#sudoku.printDomain()
+print()
